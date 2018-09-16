bfdad081fa77cf70a85a713b1128fdc5a535d1dd
Luis Hansen <reescolando@gmail.com>
2018-09-16 12:44:23 -0300

    Update cleanTrainingDatasetForCID.py

--- cleanTrainingDatasetForCID.py
+++ cleanTrainingDatasetForCID.py
@@ -1,5 +1,4 @@
 import pandas as pd
 df = pd.read_csv('hackaturing.dsv', sep='|',engine='python')
-validCid = df['base_hackaturing.cid']
-validData = df.dropna()
-df.to_csv('onlyvalidCID.csv', sep=',')
+validCid = df[~df['base_hackaturing.cid'].isna()]
+validData.to_csv('onlyvalidCID.csv', sep=',')
