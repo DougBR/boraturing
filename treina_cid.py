13f7dff2c2344df48b66f0ef22bfb95b58d88493
Douglas Luan <douglasctm@gmail.com>
2018-09-16 09:20:00 -0300

    Treinamento com 20% realizado. Problema encontrado na predição. Modelo salvo em trainedtree20train.dat

--- treina_cid.py
+++ treina_cid.py
@@ -1,10 +1,9 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
-import pickle
+from sklearn.externals import joblib
 
 
 df = pd.read_csv('onlyvalidCID.csv', sep=',')
@@ -18,7 +17,7 @@ X = df.drop('cid', axis=1)
 y = df['cid']
 del df
 file = open('Failed.py', 'w')
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.8)
 print("Iniciando treinamento")
 file.write("X_train:\n"+ str(X_train))
 file.write("\nY_train:\n"+ str(y_train))
@@ -34,26 +33,35 @@ file.write("\nY_test:\n"+ str(y_test))
 from sklearn import tree
 my_classifier = tree.DecisionTreeClassifier()
 my_classifier.fit(X_train, y_train)
-
-predictions = my_classifier.predict(X_test)
-
-from sklearn.metrics import accuracy_score
-print("Acurácia Decision Tree: ", accuracy_score(y_test, predictions))
-
+print("Fitting completed!")
 
 # Salvando máquina treinada
-filename = 'trainedtree.dat'
-pickle.dump(my_classifier, open(filename, 'wb'))
- 
+joblib.dump(my_classifier, 'trainedtree20train.dat')
+print("Model saved successfully")
+try:
+	predictions = my_classifier.predict(X_test)
+	predictions("predictions realized with success!")
+except Exception as e:
+	logger.error('Error in the pedicting fase: '+ str(e))
+finally:
+	print("Past prediction line")
 
-print("Treinando random forest")
-clf = RandomForestClassifier(n_estimators=10, max_depth=None, min_samples_split=2, random_state=0)
-clf = clf.fit(X_train, y_train)
-print("Acurácia Random Forest:", clf.score(X_test, y_test))
-filename = 'trainedRNDTree.dat'
-pickle.dump(clf, open(filename, 'wb'))
+from sklearn.metrics import accuracy_score
+print("Acurácia Decision Tree (20% treinamento): ", accuracy_score(y_test, predictions))
+
+del predictions
+
  
-# load the model from disk
+del my_classifier
+
+##print("Treinando random forest")
+##clf = RandomForestClassifier(n_estimators=10, max_depth=None, min_samples_split=2, random_state=0)
+##clf = clf.fit(X_train, y_train)
+##print("Acurácia Random Forest:", clf.score(X_test, y_test))
+##filename = 'trainedRNDTree.dat'
+##pickle.dump(clf, open(filename, 'wb'))
+## 
+### load the model from disk
 #loaded_model = pickle.load(open(filename, 'rb'))
 #result = loaded_model.score(X_test, Y_test)
 #print(result)
