f58b17294cd7e1e9d74d07b10110313f7662cce4
Douglas Luan <douglasctm@gmail.com>
2018-09-16 06:17:50 -0300

    Pickle não havia sido importado

--- treina_cid.py
+++ treina_cid.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
+import pickle
 
 
 df = pd.read_csv('onlyvalidCID.csv', sep=',')
