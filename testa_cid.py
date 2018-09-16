03deb4cdbe6a8f2622b53eee18c7a5593368bc81
Douglas Luan <douglasctm@gmail.com>
2018-09-16 12:55:09 -0300

    testa_cid fim

--- testa_cid.py
+++ testa_cid.py
@@ -4,13 +4,15 @@ import pandas as pd
 
 clf = joblib.load('trainedtree80train.dat')
 X = pd.read_csv('testData80_allfeatures1_10000.csv', sep=',')
-X.drop('base_hackaturing.cid', axis=1)
+#X.drop('base_hackaturing.cid', axis=1)
 X.fillna(0, inplace=True)
 for column in X.columns:
     if X[column].dtype == type(object):
         le = LabelEncoder()
         X[column] = le.fit_transform(X[column].astype(str))
+
 new_cids = clf.predict(X)
+
 pd.concat([X, new_cids], axis=1)
 
 X.to_csv('cidsfound1_10000', sep=',')
